b2a9a9d2ae1e612771c019632c1e983209db4870
Farooq <ofarooqdev@gmail.com>
2025-08-16 01:18:46 +0300

    [ADD]: Creating the terms of use and privacy policy

---
+++ src/components/footer/PrivacyPolicy.tsx
@@ -0,0 +1,134 @@
+"use client";
+
+import Link from "next/link";
+import { modalArticleStyles } from "@/styles/components";
+
+export default function PrivacyPolicy() {
+  return (
+    <article className={modalArticleStyles.container}>
+      <h1 className={modalArticleStyles.h1}>
+        RCA Physical Therapy Inc. Privacy Policy
+      </h1>
+
+      <p>
+        This Privacy Policy describes how your personal information is collected, used, and shared when you visit or make a purchase from{ " " }
+        <Link href="https://www.rcapt.com/" title="https://www.rcapt.com/" className={modalArticleStyles.link} target="_blank" rel="noopener noreferrer">
+          https://www.rcapt.com/
+        </Link>
+        { " " }(the <strong>"Site"</strong>).
+      </p>
+
+      <section className={modalArticleStyles.section}>
+        <h2 className={modalArticleStyles.h2}>
+          Personal Information We Collect
+        </h2>
+        <p>
+          When you visit the Site, we automatically collect certain information about your device, including information about your web browser, IP address, time zone, and some of the cookies that are installed on your device. Additionally, as you browse the Site, we collect information about the individual web pages or products that you view, what websites or search terms referred you to the Site, and information about how you interact with the Site. We refer to this automatically-collected information as <strong>"Device Information"</strong>.
+        </p>
+        <p>We collect Device Information using the following technologies:</p>
+        <ul className={modalArticleStyles.list}>
+          <li>
+            <strong>"Cookies"</strong> are data files that are placed on your device or computer and often include an anonymous unique identifier. For more information about cookies, and how to disable cookies, visit <Link href="http://www.allaboutcookies.org" title="http://www.allaboutcookies.org" className={modalArticleStyles.link} target="_blank" rel="noopener noreferrer">allaboutcookies.org</Link>.
+          </li>
+          <li>
+            <strong>"Log files"</strong> track actions occurring on the Site, and collect data including your IP address, browser type, Internet service provider, referring/exit pages, and date/time stamps.
+          </li>
+          <li>
+            <strong>"Web beacons", "tags", and "pixels"</strong> are electronic files used to record information about how you browse the Site.
+          </li>
+        </ul>
+        <p>When we talk about <strong>"Personal Information"</strong> in this Privacy Policy, we are talking about Device Information.</p>
+      </section>
+
+      <section className={modalArticleStyles.section}>
+        <h2 className={modalArticleStyles.h2}>
+          How Do We Use Your Personal Information
+        </h2>
+        <p>
+          We use the Device Information that we collect to help us screen for potential risk and fraud (in particular, your IP address), and more generally to improve and optimize our Site (for example, by generating analytics about how our customers browse and interact with the Site, and to assess the success of our marketing and advertising campaigns).
+        </p>
+      </section>
+
+      <section className={modalArticleStyles.section}>
+        <h2 className={modalArticleStyles.h2}>
+          Sharing Your Personal Information
+        </h2>
+        <p>
+          We share your Personal Information with third parties to help us use your Personal Information, as described above. We also use Google Analytics to help us understand how our customers use the Site–you can read more about how Google uses your Personal Information here:{ " " }
+          <Link href="https://www.google.com/intl/en/policies/privacy/" title="https://www.google.com/intl/en/policies/privacy/" className={modalArticleStyles.link} target="_blank" rel="noopener noreferrer">
+            Google Privacy Policy
+          </Link>
+          .
+        </p>
+        <p>
+          You can also opt-out of Google Analytics here:{ " " }
+          <Link href="https://tools.google.com/dlpage/gaoptout" title="https://tools.google.com/dlpage/gaoptout" className={modalArticleStyles.link} target="_blank" rel="noopener noreferrer">
+            here
+          </Link>
+          .
+        </p>
+        <p>Finally, we may also share your Personal Information to comply with applicable laws and regulations, to respond to a subpoena, search warrant or other lawful request for information we receive, or to otherwise protect our rights.</p>
+      </section>
+
+      <section className={modalArticleStyles.section}>
+        <h2 className={modalArticleStyles.h2}>
+          Behavioural Advertising
+        </h2>
+        <p>
+          As described above, we use your Personal Information to provide you with targeted advertisements or marketing communications we believe may be of interest to you. For more information about how targeted advertising works, you can visit{ " " }
+          <Link
+            href="http://www.networkadvertising.org/understanding-online-advertising/how-does-it-work"
+            title="http://www.networkadvertising.org/understanding-online-advertising/how-does-it-work"
+            className={ modalArticleStyles.link } target="_blank" rel="noopener noreferrer"
+          >
+            the Network Advertising Initiative's (<strong>"NAI"</strong>) educational page
+          </Link>
+          .
+        </p>
+        <p>You can opt out of targeted advertising by:</p>
+        <ul className={modalArticleStyles.list}>
+          <li>
+            <Link href="https://www.facebook.com/settings/?tab=ads" title="https://www.facebook.com/settings/?tab=ads" className={modalArticleStyles.link} target="_blank" rel="noopener noreferrer">Facebook</Link>
+          </li>
+          <li>
+            <Link href="https://www.google.com/settings/ads/anonymous" title="https://www.google.com/settings/ads/anonymous" className={modalArticleStyles.link} target="_blank" rel="noopener noreferrer">Google</Link>
+          </li>
+          <li>
+            <Link href="https://advertise.bingads.microsoft.com/en-us/resources/policies/personalized-ads" title="https://advertise.bingads.microsoft.com/en-us/resources/policies/personalized-ads" className={modalArticleStyles.link} target="_blank" rel="noopener noreferrer">Bing</Link>
+          </li>
+        </ul>
+        <p>
+          Additionally, you can opt out of some of these services by visiting{" "}
+          <Link href="http://optout.aboutads.info/" title="http://optout.aboutads.info/" className={ modalArticleStyles.link } target="_blank" rel="noopener noreferrer">the Digital Advertising Alliance's opt-out portal</Link>.
+        </p>
+      </section>
+
+      <section className={modalArticleStyles.section}>
+        <h2 className={modalArticleStyles.h2}>Do Not Track</h2>
+        <p>Please note that we do not alter our Site’s data collection and use practices when we see a Do Not Track signal from your browser.</p>
+      </section>
+
+      <section className={modalArticleStyles.section}>
+        <h2 className={modalArticleStyles.h2}>Data Retention</h2>
+        <p>When you place an order through the Site, we will maintain your Order Information for our records unless and until you ask us to delete this information.</p>
+      </section>
+
+      <section className={modalArticleStyles.section}>
+        <h2 className={modalArticleStyles.h2}>Changes</h2>
+        <p>We may update this privacy policy from time to time in order to reflect, for example, changes to our practices or for other operational, legal or regulatory reasons.</p>
+      </section>
+
+      <section className={modalArticleStyles.section}>
+        <h2 className={modalArticleStyles.h2}>Contact Us</h2>
+        <p>
+          For more information about our privacy practices, if you have questions, or if you would like to make a complaint, please contact us by email at{ " " }
+          <Link href="mailto:RCAPhysicalTherapy@gmail.com" title="RCAPhysicalTherapy@gmail.com" className={modalArticleStyles.link}>
+            RCAPhysicalTherapy@gmail.com
+          </Link>{ " " }
+          or by mail using the details provided below:
+        </p>
+        <p>8687 Connecticut St, Suite F, Merrillville, IN 46410</p>
+      </section>
+    </article>
+  );
+}
---
+++ src/components/footer/TermsOfService.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import { modalArticleStyles } from "@/styles/components";
+import Link from "next/link";
+
+export default function TermsOfService() {
+  return (
+    <article className={ modalArticleStyles.container }>
+      <h1 className={ modalArticleStyles.h1 }>
+        RCA Physical Therapy Inc. Terms of Service
+      </h1>
+
+      <p>
+        These Terms of Service (<strong>“Terms”</strong>) govern your access to and use of the services provided by RCA Physical Therapy Inc. (<strong>“we”, “our”, or “us”</strong>) through our website{ " " }
+        <Link href="https://www.rcapt.com/" title="https://www.rcapt.com/" className={ modalArticleStyles.link } target="_blank" rel="noopener noreferrer">
+          https://www.rcapt.com/
+        </Link>
+        { " " }(the <strong>“Site”</strong>) and any related services.
+      </p>
+
+      <section className={ modalArticleStyles.section }>
+        <h2 className={ modalArticleStyles.h2 }>Acceptance of Terms</h2>
+        <p>
+          By accessing or using our Site, you agree to be bound by these Terms and our Privacy Policy. If you do not agree, please do not use the Site.
+        </p>
+      </section>
+
+      <section className={ modalArticleStyles.section }>
+        <h2 className={ modalArticleStyles.h2 }>Use of the Site</h2>
+        <ul className={ modalArticleStyles.list }>
+          <li>You must be at least 18 years old or have the consent of a parent or guardian to use our services.</li>
+          <li>You agree to use the Site only for lawful purposes and not for any illegal or unauthorized activities.</li>
+          <li>You may not attempt to interfere with or disrupt the Site, servers, or networks connected to the Site.</li>
+        </ul>
+      </section>
+
+      <section className={ modalArticleStyles.section }>
+        <h2 className={ modalArticleStyles.h2 }>Appointments and Services</h2>
+        <p>
+          All appointments are subject to availability. RCA Physical Therapy Inc. reserves the right to reschedule or cancel appointments as necessary. Services are provided as described on the Site, and we do not guarantee specific outcomes.
+        </p>
+      </section>
+
+      <section className={ modalArticleStyles.section }>
+        <h2 className={ modalArticleStyles.h2 }>Payment and Billing</h2>
+        <p>
+          Payment for services must be made in full at the time of service unless otherwise agreed. We accept the payment methods listed on our Site. You are responsible for any fees or charges associated with your payment method.
+        </p>
+      </section>
+
+      <section className={ modalArticleStyles.section }>
+        <h2 className={ modalArticleStyles.h2 }>User Content</h2>
+        <p>
+          If you submit any content (e.g., feedback, reviews) to the Site, you grant us a non-exclusive, royalty-free, worldwide license to use, copy, modify, and display that content in connection with our services.
+        </p>
+      </section>
+
+      <section className={ modalArticleStyles.section }>
+        <h2 className={ modalArticleStyles.h2 }>Intellectual Property</h2>
+        <p>
+          All content on the Site, including text, graphics, logos, and software, is the property of RCA Physical Therapy Inc. or its licensors and is protected by copyright and other intellectual property laws. You may not use any content without our prior written permission.
+        </p>
+      </section>
+
+      <section className={ modalArticleStyles.section }>
+        <h2 className={ modalArticleStyles.h2 }>Limitation of Liability</h2>
+        <p>
+          To the fullest extent permitted by law, RCA Physical Therapy Inc. shall not be liable for any indirect, incidental, special, or consequential damages arising from your use of the Site or services, even if advised of the possibility of such damages.
+        </p>
+      </section>
+
+      <section className={ modalArticleStyles.section }>
+        <h2 className={ modalArticleStyles.h2 }>Governing Law</h2>
+        <p>
+          These Terms are governed by and construed in accordance with the laws of the State of Indiana, without regard to its conflict of law principles.
+        </p>
+      </section>
+
+      <section className={ modalArticleStyles.section }>
+        <h2 className={ modalArticleStyles.h2 }>Changes to Terms</h2>
+        <p>
+          We may update these Terms from time to time. Any changes will be posted on this page, and your continued use of the Site constitutes acceptance of the updated Terms.
+        </p>
+      </section>
+
+      <section className={ modalArticleStyles.section }>
+        <h2 className={ modalArticleStyles.h2 }>Contact Us</h2>
+        <p>
+          For questions regarding these Terms, please contact us by email at{ " " }
+          <Link href="mailto:RCAPhysicalTherapy@gmail.com" title="mailto:RCAPhysicalTherapy@gmail.com" className={ modalArticleStyles.link }>
+            RCAPhysicalTherapy@gmail.com
+          </Link>{ " " }
+          or by mail at:
+        </p>
+        <p>8687 Connecticut St, Suite F, Merrillville, IN 46410</p>
+      </section>
+    </article>
+  );
+}
