5bb641e781421ce875f2a66a5987410d69c6fa96
Farooq <ofarooqdev@gmail.com>
2025-08-15 23:03:32 +0300

    [UPDATE]: Allow the button component to route and use onClick prop together.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,35 +2,26 @@
 
 import React from 'react';
 import clsx from 'clsx';
+import { buttonStyles } from '@/styles/components';
 import type { ButtonProps } from "@/mytypes/compProps";
 import { useRouter } from 'next/navigation';
 
-const buttonStyles = {
-  base: clsx(
-    "rounded-xl px-4 py-2 transition-colors",
-    "text-green-700 dark:text-white",
-    "hover:text-white dark:hover:text-green-700",
-    "bg-green-100 hover:bg-green-500",
-    "dark:bg-green-700 dark:hover:bg-green-100",
-    "border-2 border-green-400 dark:border-green-600",
-    "active:scale-95 active:bg-green-600 dark:active:bg-green-200",
-    "active:text-white dark:active:text-green-700"
-  ),
-};
 
-
-function Button({ children, route, ...props }: ButtonProps) {
+function Button({ children, route, className, ...props }: ButtonProps) {
   const router = useRouter();
 
-  const handleClick = () => {
-    if (route) {
-      router.push(route);
+  // Route works even if onClick prop is passed.
+  const handleClick = ( e: React.MouseEvent<HTMLButtonElement> ) => {
+    props.onClick?.( e );
+    // Prevent default behavior if route is defined
+    if ( route && !e.defaultPrevented ) {
+      router.push( route );
     }
   };
 
   return (
     <button
-      className={ buttonStyles.base }
+      className={ clsx(buttonStyles.base, className) }
       onClick={ handleClick }
       { ...props }
     >
--- src/styles/components.ts
+++ src/styles/components.ts
@@ -1,5 +1,18 @@
 import clsx from "clsx";
 
+export const buttonStyles = {
+  base: clsx(
+    "rounded-xl px-4 py-2 transition-colors",
+    "text-green-700 dark:text-white",
+    "hover:text-white dark:hover:text-green-700",
+    "bg-green-100 hover:bg-green-500",
+    "dark:bg-green-700 dark:hover:bg-green-100",
+    "border-2 border-green-400 dark:border-green-600",
+    "active:scale-95 active:bg-green-600 dark:active:bg-green-200",
+    "active:text-white dark:active:text-green-700"
+  ),
+};
+
 export const modalStyles = {
   bg: clsx(
     "fixed inset-0 z-50",
