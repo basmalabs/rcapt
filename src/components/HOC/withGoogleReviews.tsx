7b7d0300f436d6c021b09248ac354e0b618b5755
Farooq <ofarooqdev@gmail.com>
2025-08-19 00:48:19 +0300

    [BUG]: Incorrect type definition fixed.

--- src/components/HOC/withGoogleReviews.tsx
+++ src/components/HOC/withGoogleReviews.tsx
@@ -1,9 +1,9 @@
 "use server"
 
 import { getGoogleReviews } from '@/server/utils/getGoogleReviews';
-import { rawGoogleReview } from '@/mytypes/server';
+import { googleReviews } from '@/mytypes/server';
 
-function withGoogleReviews<P extends { googleReviews: rawGoogleReview[] }>(
+function withGoogleReviews<P extends { googleReviews: googleReviews[] }>(
   WrappedComponent: React.ComponentType<P>
 ) {
   return async function ReviewsHOC( props: Omit<P, "googleReviews"> ) {
